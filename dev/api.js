f5e32dd199618b8899863ac230def7dada3f3327
Azat Sayan <sayanazat@gmail.com>
2022-05-25 16:41:50 +0300

    the POST /transaction endpoint

--- dev/api.js
+++ dev/api.js
@@ -13,7 +13,14 @@ app.get("/blockchain", function (req, res) {
   res.send(bitcoin);
 });
 
-app.post("/transaction", function (req, res) {});
+app.post("/transaction", function (req, res) {
+  const blockIndex = bitcoin.createNewTransaction(
+    req.body.amount,
+    req.body.sender,
+    req.body.recipient
+  );
+  res.json({ note: "Transaction will be added in block ${blockIndex}" });
+});
 
 app.get("/mine", function (req, res) {});
 
