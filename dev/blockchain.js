060678b93f2c30adfff747ef09229eb05c34b9de
Azat Sayan <sayanazat@gmail.com>
2022-05-25 11:30:27 +0300

    Initial commit

---
+++ dev/blockchain.js
@@ -0,0 +1,62 @@
+const sha256 = require("sha256");
+//import { sha256 } from "js-sha256";
+
+export class Blockchain {
+  constructor() {
+    this.chain = [];
+    this.pendingTransactions = [];
+  }
+}
+
+Blockchain.prototype.createNewBlock = function (
+  nonce,
+  previousBlockHash,
+  hash
+) {
+  const newBlock = {
+    index: this.chain.length + 1,
+    timestamp: Date.now(),
+    transactions: this.pendingTransactions,
+    nonce: nonce,
+    hash: hash,
+    previousBlockHash: previousBlockHash,
+  };
+
+  this.pendingTransactions = [];
+  this.chain.push(newBlock);
+
+  return newBlock;
+};
+
+Blockchain.prototype.getLastBlock = function () {
+  return this.chain[this.chain.length - 1];
+};
+
+Blockchain.prototype.createNewTransaction = function (
+  amount,
+  sender,
+  receipent
+) {
+  const newTransaction = {
+    amount: amount,
+    sender: sender,
+    receipent: receipent,
+  };
+
+  this.pendingTransactions.push(newTransaction);
+
+  return this.getLastBlock()["index"] + 1;
+};
+
+Blockchain.prototype.hashBlock = function (
+  previousBlockHash,
+  currentBlockData,
+  nonce
+) {
+  const dataAsString =
+    previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+  const hash = sha256(dataAsString);
+  return hash;
+};
+
+module.exports = Blockchain;
---
+++ dev/test.js
@@ -0,0 +1,9 @@
+import { Blockchain } from "./blockchain";
+
+const bitcoin = new Blockchain();
+
+bitcoin.createNewBlock(2345, "ODOSOOSBS90D", "90BJSDKSKBSS");
+
+bitcoin.createNewTransaction(3663, "EURURUEI", "REUUUEUEUUE");
+
+console.log(bitcoin);
